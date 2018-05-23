b646941ec4f0ba054f1245608935133eca689ddd
Damian Marius <damian.mm@live.com>
2018-05-23 09:35:07 -0700

    added POST endpoints

--- server.js
+++ server.js
@@ -32,20 +32,33 @@ app.get('/shopping-list', (req, res) => {
 });
 
 app.post('/shopping-list', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
+    const neededFields = ['name', 'budget'];
+    for (let i=0; i<neededFields.length; i++) {
+      const field = neededFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
     }
-  }
-
-  const item = ShoppingList.create(req.body.name, req.body.budget);
-  res.status(201).json(item);
-});
+    const item = ShoppingList.create(req.body.name, req.body.budget);
+    res.status(201).json(item);
+  });
+  
+  app.post('/recipes', jsonParser, (req, res) => {
+    const neededFields = ['name', 'ingredients'];
+    for (let i=0; i<neededFields.length; i++) {
+      const field = neededFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    const item = Recipes.create(req.body.name, req.body.ingredients);
+    res.status(201).json(item);
+  });
+  
 
 
 app.get('/recipes', (req, res) => {
